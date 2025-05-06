aa2f26917f810110f99aef26df9dea96401bf06c
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-05-06 10:18:15 +0200

    fix: no inline styling

--- plugin/src/main.ts
+++ plugin/src/main.ts
@@ -4,12 +4,13 @@ import {
 	Editor,
 	MarkdownFileInfo,
 	MarkdownView,
-	Modal, Notice,
+	Modal,
+	Notice,
 	Plugin,
 	PluginSettingTab,
 	Setting,
-	TFile
-} from 'obsidian';
+	TFile,
+} from "obsidian";
 
 interface RecursiveShareSettings {
 	serverUri: string;
@@ -17,70 +18,79 @@ interface RecursiveShareSettings {
 }
 
 const DEFAULT_SETTINGS: RecursiveShareSettings = {
-	serverUri: '',
-	apiKey: '',
-}
+	serverUri: "",
+	apiKey: "",
+};
 
 type FileToUpload = {
 	name: string;
-} & (
-	{ tfile: TFile; }
-	| { value: string; }
-	)
+} & ({ tfile: TFile } | { value: string });
 
 type ShareMetadata = {
 	id: string;
 	title: string;
 	created_at: string;
-}
+};
 
-async function createShare(config: RecursiveShareSettings, name: string): Promise<[string, string]> {
+async function createShare(
+	config: RecursiveShareSettings,
+	name: string,
+): Promise<[string, string]> {
 	let share: ShareMetadata = await fetch(`${config.serverUri}/api/create`, {
 		headers: {
 			"Content-Type": "application/json",
-			"Authorization": config.apiKey
+			Authorization: config.apiKey,
 		},
 		method: "POST",
 		body: JSON.stringify({
-			root_name: name
-		})
-	}).then(res => res.json())
+			root_name: name,
+		}),
+	}).then((res) => res.json());
 
-	let hyphenated_title = (share.title.split("/").last() || "").replace(/[^a-zA-Z0-9]/g, "-")
-	let link = `${config.serverUri}/view/${share.id}#-${hyphenated_title}`
+	let hyphenated_title = (share.title.split("/").last() || "").replace(
+		/[^a-zA-Z0-9]/g,
+		"-",
+	);
+	let link = `${config.serverUri}/view/${share.id}#-${hyphenated_title}`;
 
 	return [link, share.id];
 }
 
-async function uploadFiles(files: FileToUpload[], config: RecursiveShareSettings, id: string) {
+async function uploadFiles(
+	files: FileToUpload[],
+	config: RecursiveShareSettings,
+	id: string,
+) {
 	for (const file of files) {
 		let fileContent: any;
-		if ('tfile' in file) {
+		if ("tfile" in file) {
 			fileContent = await this.app.vault.readBinary(file.tfile);
 		} else {
 			fileContent = file.value;
 		}
 		const formData = new FormData();
 		const blob = new Blob([fileContent]);
-		formData.append('file', blob, file.name);
-		formData.append('name', file.name);
+		formData.append("file", blob, file.name);
+		formData.append("name", file.name);
 
 		const response = await fetch(`${config.serverUri}/api/${id}`, {
-			method: 'PUT',
+			method: "PUT",
 			headers: {
-				"Authorization": config.apiKey
+				Authorization: config.apiKey,
 			},
-			body: formData
+			body: formData,
 		});
 
 		if (!response.ok) {
-			throw new Error(`Failed to upload file ${file.name}: ${response.statusText}`);
+			throw new Error(
+				`Failed to upload file ${file.name}: ${response.statusText}`,
+			);
 		}
 
-		new Notice("Upload of " + file.name + " complete.")
+		new Notice("Upload of " + file.name + " complete.");
 	}
 
-	new Notice("--- Upload complete ---")
+	new Notice("--- Upload complete ---");
 }
 
 export default class RecursiveSharePlugin extends Plugin {
@@ -90,59 +100,77 @@ export default class RecursiveSharePlugin extends Plugin {
 		await this.loadSettings();
 
 		this.addCommand({
-			id: 'share',
-			name: 'Share recursively',
-			editorCallback: async (editor: Editor, ctx: MarkdownView | MarkdownFileInfo) => {
+			id: "share",
+			name: "Share recursively",
+			editorCallback: async (
+				editor: Editor,
+				ctx: MarkdownView | MarkdownFileInfo,
+			) => {
 				let view = ctx as MarkdownView;
 				let info = ctx as MarkdownFileInfo;
-				let name = info.file!.name
-				let content = editor.getValue()
+				let name = info.file!.name;
+				let content = editor.getValue();
 				let file = view.file!;
-				let cache = this.app.metadataCache.getFileCache(file)!
+				let cache = this.app.metadataCache.getFileCache(file)!;
 
-				let resourcesToUpload: FileToUpload[] = []
+				let resourcesToUpload: FileToUpload[] = [];
 
 				if (cache.embeds) {
 					for (const embed of cache.embeds) {
-						let destFile = this.app.metadataCache.getFirstLinkpathDest(embed.link, file.path);
+						let destFile =
+							this.app.metadataCache.getFirstLinkpathDest(
+								embed.link,
+								file.path,
+							);
 						if (!destFile) continue;
 						let destName = embed.link;
-						while (destName.startsWith("../") || destName.startsWith("..\\")) {
+						while (
+							destName.startsWith("../") ||
+							destName.startsWith("..\\")
+						) {
 							destName = destName.substring(3);
 						}
-						if (destName.startsWith("/") || destName.startsWith("\\")) {
+						if (
+							destName.startsWith("/") ||
+							destName.startsWith("\\")
+						) {
 							destName = destName.substring(1);
 						}
 						resourcesToUpload.push({
 							name: destName,
-							tfile: destFile
-						})
+							tfile: destFile,
+						});
 
 						let urlencoded = encodeURIComponent(destName);
-						content = content.replace(embed.original, `![${embed.displayText}](${this.settings.serverUri}/api/##id##/${urlencoded})`)
+						content = content.replace(
+							embed.original,
+							`![${embed.displayText}](${this.settings.serverUri}/api/##id##/${urlencoded})`,
+						);
 					}
 				}
 
-				const [link, id] = await createShare(this.settings, name)
+				const [link, id] = await createShare(this.settings, name);
 				resourcesToUpload.push({
 					name: name,
-					value: content.replace(/##id##/g, id)
-				})
-				await uploadFiles(resourcesToUpload, this.settings, id)
+					value: content.replace(/##id##/g, id),
+				});
+				await uploadFiles(resourcesToUpload, this.settings, id);
 				new ShareLinkModal(this.app, link).open();
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new RecursiveShareSettingsTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
 
 	async saveSettings() {
@@ -159,40 +187,42 @@ class RecursiveShareSettingsTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Root Server URL')
-			.setDesc('You\'ll need a hosted instance')
-			.addText(text => text
-				.setPlaceholder('Enter your URI')
-				.setValue(this.plugin.settings.serverUri)
-				.onChange(async (value) => {
-					this.plugin.settings.serverUri = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Root Server URL")
+			.setDesc("You'll need a hosted instance")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your URI")
+					.setValue(this.plugin.settings.serverUri)
+					.onChange(async (value) => {
+						this.plugin.settings.serverUri = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 
 		new Setting(containerEl)
 			.setName("API Key")
 			.setDesc("See the linked guide for more information")
-			.addText(text => text
-				.setPlaceholder("Enter your API key")
-				.setValue(this.plugin.settings.apiKey)
-				.onChange(async (value) => {
-					this.plugin.settings.apiKey = value;
-					await this.plugin.saveSettings();
-				}));
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your API key")
+					.setValue(this.plugin.settings.apiKey)
+					.onChange(async (value) => {
+						this.plugin.settings.apiKey = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 
-
-		containerEl.createEl('a', {
-			text: 'Click here for a guide on how to host an instance',
-			href: 'https://github.com/Bloeckchengrafik/obsidian-recursive-share'
+		containerEl.createEl("a", {
+			text: "Click here for a guide on how to host an instance",
+			href: "https://github.com/Bloeckchengrafik/obsidian-recursive-share",
 		});
 	}
 }
-
 
 export class ShareLinkModal extends Modal {
 	linkToShare: string;
@@ -203,43 +233,40 @@ export class ShareLinkModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 
 		// Add the main text
-		contentEl.createEl('p', {text: "Here's your share link:"});
+		contentEl.createEl("p", { text: "Here's your share link:" });
 
 		// Add an input field to display the link and make it easy to select/copy
-		const linkInput = contentEl.createEl('input', {
-			type: 'text',
+		const linkInput = contentEl.createEl("input", {
+			type: "text",
 			value: this.linkToShare,
-			cls: 'share-link-input' // Optional: Add a class for potential styling
+			cls: "share-link-input", // Optional: Add a class for potential styling
 		});
-		linkInput.setAttr('readonly', 'true'); // Make it read-only so user doesn't accidentally edit
-
-		// Optional: Add some basic styling for the input field
-		linkInput.style.width = '100%';
-		linkInput.style.padding = '5px';
-		linkInput.style.marginTop = '10px';
-		linkInput.style.marginBottom = '10px';
+		linkInput.setAttr("readonly", "true"); // Make it read-only so user doesn't accidentally edit
 
 		// Add a button to copy the link
 		new ButtonComponent(contentEl)
 			.setButtonText("Copy Link")
 			.setCta() // Apply call-to-action styling
 			.onClick(() => {
-				navigator.clipboard.writeText(this.linkToShare).then(() => {
-					// Optionally, provide feedback to the user
-					this.close(); // Close the modal after copying
-					// You could also add a confirmation message instead of closing immediately
-				}).catch(err => {
-					console.error('Failed to copy link: ', err);
-					// Optionally, show an error message to the user
-				});
+				navigator.clipboard
+					.writeText(this.linkToShare)
+					.then(() => {
+						// Optionally, provide feedback to the user
+						this.close(); // Close the modal after copying
+						// You could also add a confirmation message instead of closing immediately
+					})
+					.catch((err) => {
+						console.error("Failed to copy link: ", err);
+						// Optionally, show an error message to the user
+					});
 			});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty(); // Clean up the modal content
 	}
 }
